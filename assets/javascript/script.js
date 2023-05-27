15705fd6bd5be402fe448b64f0064d0eef941961
Stefan_Caramizoiu <stefancaramizoiu@gmail.com>
2023-05-27 12:49:57 +0300

    Code refactoring

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,98 +6,45 @@ function disableBut() {
 }
 
 function playerWin(n) {
+    let message;
     if (n === "x") {
-        document.getElementById('turn').innerHTML = "Player X won";
-        disableBut();
+        message = "Player X won";
     } else if (n === "0") {
-        document.getElementById('turn').innerHTML = "Player 0 won";
-        disableBut();
+        message = "Player 0 won";
     } else {
-        document.getElementById('turn').innerHTML = "Draw game";
+        message = "Draw game";
     }
+    document.getElementById('turn').innerHTML = message;
+    disableBut();
 }
+
 // function for user click on a tab
 function check() {
-
-    //get values of buttons
-    var b1, b2, b3, b4, b5, b6, b7, b7, b8, b9
-    b1 = document.getElementById("b1").value;
-    b2 = document.getElementById("b2").value;
-    b3 = document.getElementById("b3").value;
-    b4 = document.getElementById("b4").value;
-    b5 = document.getElementById("b5").value;
-    b6 = document.getElementById("b6").value;
-    b7 = document.getElementById("b7").value;
-    b8 = document.getElementById("b8").value;
-    b9 = document.getElementById("b9").value;
-
-    //check if Player X won
-    if ((b1 == "x") && (b2 == "x") && (b3 == "x")) {
-        playerWin("x");
-        
-    } else if ((b1 == "x") && (b4 == "x") && (b7 == "x")) {
-        playerWin("x");
-       
-    } else if ((b7 == "x") && (b8 == "x") && (b9 == "x")) {
-        playerWin("x");
-       
-    } else if ((b3 == "x") && (b6 == "x") && (b9 == "x")) {
-        playerWin("x");
-       
-    } else if ((b1 == "x") && (b5 == "x") && (b9 == "x")) {
-        playerWin("x");
-      
-    } else if ((b3 == "x") && (b5 == "x") && (b7 == "x")) {
-        playerWin("x");
-        
-    } else if ((b2 == "x") && (b5 == "x") && (b8 == "x")) {
-        playerWin("x");
-       
-    } else if ((b4 == "x") && (b5 == "x") && (b6 == "x")) {
-        playerWin("x");
-       
-    } 
-    //check if Player 0 won
-    else if ((b1 == "0") && (b2 == "0") && (b3 == "0")) {
-        playerWin("0");
-       
-    } else if ((b1 == "0") && (b4 == "0") && (b7 == "0")) {
-        playerWin("0");
-       
-    } else if ((b7 == "0") && (b8 == "0") && (b9 == "0")) {
-        playerWin("0");
-       
-    } else if ((b3 == "0") && (b6 == "0") && (b9 == "0")) {
-        playerWin("0");
-      
-    } else if ((b1 == "0") && (b5 == "0") && (b9 == "0")) {
-        playerWin("0");
-       
-    } else if ((b3 == "0") && (b5 == "0") && (b7 == "0")) {
-        playerWin("0");
-       
-    } else if ((b2 == "0") && (b5 == "0") && (b8 == "0")) {
-        playerWin("0");
-     
-    } else if ((b4 == "0") && (b5 == "0") && (b6 == "0")) {
-        playerWin("0");
-      
-    }
-    // check if the game is a draw
-    else if ((b1 == "x" || b1 == "0") && (b2 == "x" || b2 == "0") &&
-            (b3 == "x" || b3 == "0") && (b4 == "x" || b4 == "0") &&
-            (b5 == "x" || b5 == "0") && (b6 == "x" || b6 == "0") &&
-            (b7 == "x" || b7 == "0") && (b8 == "x" || b8 == "0") &&
-            (b9 == "x" || b9 == "0")) {
-                playerWin("draw");
-    } else {
-        //player turn
-        if (flag == 1) {
-            document.getElementById('turn').innerHTML = "Player X Turn";
-        } else {
-            document.getElementById('turn').innerHTML = "Player 0 Turn";
+    let board = [];
+    for (var i = 1; i <= 9; i++) {
+        board[i] = document.getElementById("b" + i).value;
+      }
+    let winningCombinations = [
+        [1, 2, 3], [1, 4, 7], [7, 8, 9], [3, 6, 9],
+        [1, 5, 9], [3, 5, 7], [2, 5, 8], [4, 5, 6]
+      ];
+      for (var i = 0; i < winningCombinations.length; i++) {
+        var [a, b, c] = winningCombinations[i];
+        if (board[a] === "x" && board[b] === "x" && board[c] === "x") {
+          playerWin("x");
+          return;
+        } else if (board[a] === "0" && board[b] === "0" && board[c] === "0") {
+          playerWin("0");
+          return;
         }
-    }
+      }
+    
+      if (board.slice(1).every(val => val === "x" || val === "0")) {
+        playerWin("draw");
+      } else {
+        document.getElementById('turn').innerHTML = "Player " + (flag == 1 ? "X" : "0") + " Turn";
+      }
+
 }
 //reset game function
 function reset_game() {
